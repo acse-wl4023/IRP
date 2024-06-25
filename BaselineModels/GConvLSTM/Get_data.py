3c88d7b02d36230791c7acb279691608746dda39
Sibo Cheng <scheng1@ese-datalearning>
2024-06-25 21:37:36 +0100

    Complete GConvLSTM

--- BaselineModels/GConvLSTM/Get_data.py
+++ BaselineModels/GConvLSTM/Get_data.py
@@ -1,5 +1,7 @@
 import os
 import numpy as np
+import torch
+from torch.utils.data import Dataset
 
 def extract_timestep(filename):
     base = os.path.splitext(filename)[0]  # 移除.npy后缀
@@ -34,3 +36,47 @@ def get_all_nodes(folders):
         len_of_each_case.append(length)
         all_data.append(data)
     return np.concatenate(all_data, axis=0), len_of_each_case
+
+
+def segment_data(data, len_of_each_case, window_size=10, step_size=10):
+    all_data = []
+    for i in range(len(len_of_each_case)-1):
+        for j in range(len_of_each_case[i], len_of_each_case[i+1]-window_size+1, step_size):
+            all_data.append(np.expand_dims(data[j:j+window_size], axis=0))
+        # print("case: ", i, ", length: ", len(all_data))
+        if (len_of_each_case[i+1]-window_size-len_of_each_case[i]) % step_size != 0:
+            all_data.append(np.expand_dims(data[len_of_each_case[i+1]-window_size:len_of_each_case[i+1]], axis=0))
+    return np.concatenate(all_data, axis=0)
+
+
+class Preprocessor:
+    def __init__(self):
+        self.max = None
+        self.min = None
+
+    def fit(self, data):
+        self.max = np.max(data, axis=(0, 1, 2))
+        self.min = np.min(data, axis=(0, 1, 2))
+
+    def transform(self, x):
+        transformed_data = x/self.max
+
+        return transformed_data
+
+    def inverse_transform(self, x):
+        recon_data = x*self.max
+
+        return recon_data
+    
+class MyDataset(Dataset):
+    def __init__(self, x_nodes, edge_index, edge_weight, y_nodes):
+        self.x_nodes = x_nodes
+        self.edge_index = edge_index
+        self.edge_weight = edge_weight
+        self.y_nodes = y_nodes
+
+    def __len__(self):
+        return self.x_nodes.shape[0]
+
+    def __getitem__(self, idx):
+        return self.x_nodes[idx], self.edge_index, self.edge_weight, self.y_nodes[idx]
--- BaselineModels/GConvLSTM/train.py
+++ BaselineModels/GConvLSTM/train.py
@@ -4,23 +4,6 @@ import Get_data as Gd
 import os
 import numpy as np
 
-# # Example setup for inputs
-# num_nodes = 97149
-# num_node_features = 5
-# num_classes = 3
-# edge_index = torch.tensor([[0, 1, 1, 2],
-#                            [1, 0, 2, 1]], dtype=torch.long)  # Sample connectivity
-# x = torch.randn((num_nodes, num_node_features), dtype=torch.float)
-# h = torch.zeros((num_nodes, num_node_features), dtype=torch.float)
-# c = torch.zeros((num_nodes, num_node_features), dtype=torch.float)
-
-# # Create an instance of the model
-# model = GConvLSTMCell(num_node_features, 12)
-
-# # Use torchinfo to summarize the model
-# # Pass inputs as a tuple since your forward method expects two arguments
-# summary(model, input_data=(x, edge_index, h, c))
-
 directory = '/scratch_dgxl/sc4623/wl4023/Sibo_22Mar2024'
 folders = [os.path.join(directory, f, 'hessian_') for f in os.listdir(directory) if f.startswith('case_')]
 files = [os.path.join(folders[0], f) for f in os.listdir(folders[0]) if
@@ -28,16 +11,3 @@ files = [os.path.join(folders[0], f) for f in os.listdir(folders[0]) if
 
 nodes = Gd.read_data(files)
 print(folders[0], np.max(nodes, axis=(0, 1)))
-
-# data_list = []
-# for sim in range(30):
-#     for steps in range(1,100):
-#         try:
-#             x = np.load('/scratch_dgxl/sc4623/wl4023/Sibo_22Mar2024/case_0/hessian_/foward_soln_timestep_'+'{0:g}'
-#                         .format(float("{:.2f}".format(steps*0.02)))+'.npy')
-#             print(x.shape, steps)
-#             data_list.append(x)
-#         except:
-#             pass
-
-# print(np.array(data_list).shape)
