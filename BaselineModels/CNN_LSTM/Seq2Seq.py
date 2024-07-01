1f22d7c95a8d59568a50b6d6e08a99da0deb9bc0
wl4023 <weiyi.li23@imperial.ac.uk>
2024-07-01 10:53:47 +0100

    modify .gitignore

--- BaselineModels/CNN_LSTM/Seq2Seq.py
+++ BaselineModels/CNN_LSTM/Seq2Seq.py
@@ -1,109 +1,114 @@
-import torch
 import torch.nn as nn
-import torch_geometric.nn as gnn
-from torch_geometric.nn import GCNConv
-import torch.nn.functional as F
+import torch
 
-class GConvLSTMCell(torch.nn.Module):
-    def __init__(self, input_channel, hidden_channel):
-        super(GConvLSTMCell, self).__init__()
-        self.input_dim = input_channel
-        self.hidden_dim = hidden_channel
-        self.conv = GCNConv(self.input_dim+self.hidden_dim, 4*self.hidden_dim)
-
-    def forward(self, x, edge_index, h_cur, c_cur, edge_weight = None):
-        combined = torch.cat((x, h_cur), axis=1)
-        combined_conv = self.conv(combined, edge_index, edge_weight)
-        cc_f,cc_i,cc_o,cc_g = torch.split(combined_conv, self.hidden_dim, dim=1)
-
-        f = torch.sigmoid(cc_f)
-        i = torch.sigmoid(cc_i)
-        o = torch.sigmoid(cc_o)
-        g = torch.tanh(cc_g)
-
-        c_next = f*c_cur + i*g
-        h_next = o*torch.tanh(c_next)
-
-        return h_next, c_next
-    
 
 class Encoder(nn.Module):
-    def __init__(self, seq_len, input_channels, hidden_channels, device):
+    def __init__(self, input_size, hidden_size):
         super(Encoder, self).__init__()
-        self.seq_len = seq_len
-        self.input_channels = input_channels
-        self.hidden_channels = hidden_channels
-        self.device = device
-
-        cellList = []
-        for _ in range(seq_len):
-            cellList.append(GConvLSTMCell(self.input_channels, self.hidden_channels))
+        self.input_size = input_size
+        self.hidden_size = hidden_size
         
-        self.cellList = nn.ModuleList(cellList)
+        self.sigmoid = nn.Sigmoid()
+        
+        self.W_xi = nn.Linear(input_size, hidden_size, bias=True)
+        self.W_hi = nn.Linear(hidden_size, hidden_size, bias=True)
+        
+        self.W_xf = nn.Linear(input_size, hidden_size, bias=True)
+        self.W_hf = nn.Linear(hidden_size, hidden_size, bias=True)
+        
+        self.W_xo = nn.Linear(input_size, hidden_size, bias=True)
+        self.W_ho = nn.Linear(hidden_size, hidden_size, bias=True)
+        
+        self.W_xc = nn.Linear(input_size, hidden_size, bias=True)
+        self.W_hc = nn.Linear(hidden_size, hidden_size, bias=True)
+        
+    def init_hidden(self, batch_size, device):
+        return torch.zeros(batch_size, self.hidden_size, device=device), torch.zeros(batch_size, self.hidden_size, device=device)
     
-    def init_hidden_state(self, num_nodes):
-        h = torch.zeros((num_nodes, self.hidden_channels), dtype=torch.float, device=self.device)
-        c = torch.zeros((num_nodes, self.hidden_channels), dtype=torch.float, device=self.device)
-        return h, c
-
-    def forward(self, x, edge_index, edge_weight=None):
-        num_nodes= x.shape[1]
-        h_cur, c_cur = self.init_hidden_state(num_nodes)
-        output_hList = []
-        for i in range(self.seq_len):
-            h_cur, c_cur = self.cellList[i](x[i, :, :], edge_index, h_cur, c_cur, edge_weight)
-            output_hList.append(h_cur)
-
-        output = torch.stack(output_hList, dim=0)
-        return output, (h_cur, c_cur)
-
-
+    def forward(self, x):
+        batch_size, seq_len, _ = x.shape
+        h_t, c_t = self.init_hidden(batch_size, x.device)
+        for t in range(seq_len):
+            x_t = x[:, t, :]
+            i_t = self.sigmoid(self.W_xi(x_t)+self.W_hi(h_t))
+            f_t = self.sigmoid(self.W_xf(x_t)+self.W_hf(h_t))
+            o_t = self.sigmoid(self.W_xo(x_t)+self.W_ho(h_t))
+            g_t = torch.tanh(self.W_xc(x_t)+self.W_hc(h_t))
+            
+            c_t = f_t*c_t+i_t*g_t
+            h_t = o_t * torch.tanh(c_t)
+        
+        return h_t, c_t
+            
+        
+    
 class Decoder(nn.Module):
-    def __init__(self, seq_len, hidden_channels, output_channels):
+    def __init__(self, hidden_size, output_size, output_seq_len):
         super(Decoder, self).__init__()
-        self.seq_len = seq_len
-        self.output_channels = output_channels
-        self.hidden_channels = hidden_channels
-
-        cellList = []
-        for _ in range(seq_len):
-            cellList.append(GConvLSTMCell(self.hidden_channels, self.output_channels))
-        
-        self.cellList = nn.ModuleList(cellList)
-
-        self.conv_h = GCNConv(self.hidden_channels, self.output_channels)
-        self.conv_c = GCNConv(self.hidden_channels, self.output_channels)
-
-
-    def forward(self, x, c, edge_index, edge_weight=None):
-        h_cur = self.conv_h(x, edge_index, edge_weight)
-        c_cur = self.conv_c(c, edge_index, edge_weight)
-
-        output_hList = []
-        for i in range(self.seq_len):
-            h_cur, c_cur = self.cellList[i](x, edge_index, h_cur, c_cur, edge_weight)
-            output_hList.append(h_cur)
-
-        output = torch.stack(output_hList, dim=0)
-        return output, (h_cur, c_cur)
-
-
-class GConvLSTM(nn.Module):
-    def __init__(self, input_seq_len, output_seq_len, channels, hidden_channels, device):
-        super(GConvLSTM, self).__init__()
-        self.input_seq_len = input_seq_len
+        self.hidden_size = hidden_size
+        self.output_size = output_size
         self.output_seq_len = output_seq_len
-        self.channels = channels
-        self.hidden_channels = hidden_channels
-        self.device = device
+        
+        self.sigmoid = nn.Sigmoid()
+        
+        self.W_xi = nn.Linear(self.hidden_size, self.output_size, bias=True)
+        self.W_hi = nn.Linear(self.output_size, self.output_size, bias=True)
+        
+        self.W_xf = nn.Linear(self.hidden_size, self.output_size, bias=True)
+        self.W_hf = nn.Linear(self.output_size, self.output_size, bias=True)
+        
+        self.W_xo = nn.Linear(self.hidden_size, self.output_size, bias=True)
+        self.W_ho = nn.Linear(self.output_size, self.output_size, bias=True)
+        
+        self.W_xc = nn.Linear(self.hidden_size, self.output_size, bias=True)
+        self.W_hc = nn.Linear(self.output_size, self.output_size, bias=True)
 
-        self.encoder = Encoder(self.input_seq_len, self.channels, self.hidden_channels, self.device)
-        self.decoder = Decoder(self.output_seq_len, self.hidden_channels, self.channels)
+        self.ini_h = nn.Linear(self.hidden_size, self.output_size)
+        self.ini_c = nn.Linear(self.hidden_size, self.output_size)
 
-        self.leakyRelu = nn.LeakyReLU(0.01)
-    def forward(self, x, edge_index, edge_weight):
-        _, (x, c_cur) = self.encoder(x, edge_index, edge_weight)
-        output, _ = self.decoder(x, c_cur, edge_index, edge_weight)
-        output = self.leakyRelu(output)
-        return output
+
     
+    def forward(self, x, h, c):
+        
+        h_t = self.ini_h(h)
+        c_t = self.ini_c(c)
+
+        output = []
+        for t in range(self.output_seq_len):
+            
+            i_t = self.sigmoid(self.W_xi(x)+self.W_hi(h_t))
+            f_t = self.sigmoid(self.W_xf(x)+self.W_hf(h_t))
+            o_t = self.sigmoid(self.W_xo(x)+self.W_ho(h_t))
+            g_t = torch.tanh(self.W_xc(x)+self.W_hc(h_t))
+            
+            c_t = f_t*c_t+i_t*g_t
+            h_t = o_t * torch.tanh(c_t)
+            
+            output.append(h_t.unsqueeze(1))
+        output = torch.cat(output, dim=1)
+        return output
+
+
+
+class Seq2Seq(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size, output_seq_len):
+        super(Seq2Seq, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.output_seq_len = output_seq_len
+        
+        self.encoder = Encoder(self.input_size, self.hidden_size)
+        self.decoder = Decoder(self.hidden_size, self.output_size, self.output_seq_len)
+        
+        self.leakyrelu = nn.LeakyReLU(0.1)
+    
+    def forward(self, x):
+        h_t, c_t = self.encoder(x)
+
+        output = self.decoder(h_t, h_t, c_t)
+        
+        output = self.leakyrelu(output)
+        
+        return output
+        
